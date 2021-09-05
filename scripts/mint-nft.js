e8eb8c2f657fd6a2dd653755b5233e50e50cad4b
ponyolovesham <ponyogreenbucket@gmail.com>
2021-09-05 18:27:42 -0400

    mint first NFT

---
+++ scripts/mint-nft.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const { API_URL, PUBLIC_KEY, PRIVATE_KEY } = process.env;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+// console.log(JSON.stringify(contract.abi));
+
+const contractAddress = "0xF5568656188bB2E256780485AFB42C0F70Effd33";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    maxPriorityFeePerGas: 1999999987,
+    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+  };
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err);
+    });
+}
+
+mintNFT(
+  "https://gateway.pinata.cloud/ipfs/QmbwXFtBh4p2YFNVsxqUbvorsJebRXrt4Bow8GniPeYi2v"
+);
